f4307c48f2b1607e8096b5e807e2791e58c0f493
Basil <vasileiosmichailidis@protonmail.com>
2025-03-09 17:31:30 +0200

    most recent

--- config.def.h
+++ config.def.h
@@ -4,14 +4,16 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"roboto:bold:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#af1b3f" },
-	[SchemeOut] = { "#000000", "#af1b3f" },
+			/*  fg         bg       */
+//	[SchemeNorm] = { "#915867", "#172832" },
+//	[SchemeSel] = { "#172832", "#915867" },
+	[SchemeNorm] = { "#adebfa", "#310f16" },
+	[SchemeSel] = { "#310f16", "#adebfa" },
+	[SchemeOut] = { "#000000", "#adebfa" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
